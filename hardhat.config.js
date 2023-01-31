ee981c349e1aa24430ec681a09164dcfcf29b9f2
Ashwin Prasad <ashwin@onpar.in>
2023-01-31 19:57:51 +0530

    hyperspace: DEP

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,27 @@
-require("@nomicfoundation/hardhat-toolbox");
-require('hardhat-deploy');
-require('hardhat-deploy-ethers');
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
 require("./tasks")
 require("dotenv").config()
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.17",
-  defaultNetwork: "wallaby",
-  networks: {
-    wallaby: {
-      url: "https://wallaby.node.glif.io/rpc/v0",
-      accounts: [PRIVATE_KEY],
-    }
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-};
+    solidity: "0.8.17",
+    defaultNetwork: "hyperspace",
+    networks: {
+        hyperspace: {
+            chainId: 3141,
+            url: "https://api.hyperspace.node.glif.io/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+}
+
