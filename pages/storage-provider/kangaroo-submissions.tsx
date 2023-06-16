5becaadecda152f934139d897ce6e6f3c6d885d2
kangaroo-data-dao <119771802+kangaroo-data-dao@users.noreply.github.com>
2023-06-16 12:22:26 +0530

    Update kangaroo-submissions.tsx

--- pages/storage-provider/kangaroo-submissions.tsx
+++ pages/storage-provider/kangaroo-submissions.tsx
@@ -6,6 +6,7 @@ import Header from "../../components/common/Header";
 import { useRouter } from "next/router";
 import { Dialog } from "@headlessui/react";
 
+// Handle Submission
 const KangarooSubmissions = () => {
   const {
     state: {},
